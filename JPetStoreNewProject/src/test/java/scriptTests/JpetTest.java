b5e53118490827ed65a7c5ae6815901b787a78e0
JFV <jfvilay@qualixo.com>
2018-02-14 11:52:52 +0100

    Correction suite à merge de maintenance vers master

--- JPetStoreNewProject/src/test/java/scriptTests/JpetTest.java
+++ JPetStoreNewProject/src/test/java/scriptTests/JpetTest.java
@@ -51,12 +51,12 @@ public class JpetTest {
 					
 	}
 	
-<<<<<<< HEAD
+
 	//Ajout commentaire Master
-=======
+
 	// Ajout commentaire maintenance 
 	
->>>>>>> refs/heads/maintenance
+
 	
 	@After
 	public void quit() {
